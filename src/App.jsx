7aa726fbfc9e73799fc8c5b689c58b3ff823463b
Christian Farias Aguila <christian.farias.aguila@gmail.com>
2024-04-17 20:33:15 -0400

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div></div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
